5f5a00bee9c7eae58a136ff5f01fa5bf571fc985
Puscas <cristian_puscas_98@yahoo.com>
2021-03-23 19:24:26 +0200

    reinitalized project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Oh, HI VENOMOUS!
+          Hey!
         </p>
         <a
           className="App-link"
